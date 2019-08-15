2835a6432d1231c6bfb57d9fe5c81eb26a35dcb7
Odeleye, Diana (Associate Software Developer) <diana.odeleye@sky.uk>
2019-08-15 16:35:54 +0100

    using hooks with functional components to obtain course data

--- ps-flux/src/components/CoursesPage.js
+++ ps-flux/src/components/CoursesPage.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component {
-    state = { 
-        courses : [],
-    };
+function CoursesPage() {
+    const [courses, setCourses ] = useState([]);
 
-    componentDidMount(){
-         getCourses().then(coursesData => this.setState({courses: coursesData}))
+// Similar to componentDidMount and componentDidUpdate
+// A dependency needs to be included as an argument or it will run every time the app renders
+// In this case, the app runs the effect only once due to the empty array
+
+useEffect(() => { 
+    getCourses().then(coursesData => setCourses(coursesData));
+}, []);
+
+   function renderRow(course) {
+        return(
+            <tr key={course.id}>
+                <td>{course.title}</td>
+                <td>{course.category}</td>
+                <td>{course.authorId}</td>
+            </tr>
+        )
     }
 
-    render() {
-        console.log(this.state);
-        const courses = this.state.courses;
         return (
             <>
                 <h2>Courses</h2>
@@ -25,16 +34,11 @@ class CoursesPage extends React.Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {courses.map((course, i) => <tr key={i}>
-                                <td>{course.title}</td>
-                                <td>{course.category}</td>
-                                <td>{course.authorId}</td>
-                        </tr>)}  
+                        {courses.map(renderRow)}  
                     </tbody>         
                 </table>
             </>
         )
     }
-}
 
 export default CoursesPage;
